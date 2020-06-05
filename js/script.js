8ba8afcd6ca54e68b6a9cca78028983b8688df80
taki-ikat <show13k.t@icloud.com>
2020-06-06 01:22:12 +0900

    Updated files

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-// PhotoSwipe
-initPhotoSwipeFromDOM(".js-my-gallery");
-
 $(function () {
-
-  //iOS対策
-  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
-  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
-  var ua = navigator.userAgent;
-  if (/iPhone|iPad|iPod/.test(ua)) {
-    $("body").addClass("ios");
-  }
-
-  //Worksのリンクを有効化
-  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
-  $(".works-url").on("click", "a", function (e) {
-    e.stopPropagation();
-  });
-
   //ページ内スクロール
   var $nav = $(".gnav");
   var navHeight = $nav.outerHeight();
